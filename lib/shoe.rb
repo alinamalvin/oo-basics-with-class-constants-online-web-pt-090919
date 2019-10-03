b15cb32720be20a3c520a75d702ea1be909f86d4
Alina Kornilova <alinakornilova78@gmail.com>
2019-10-03 17:01:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,25 +1,14 @@
 attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-BRANDS = []
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
-    puts "#{brand}"
-    if BRANDS.include?(brand)
-      puts "exists!"
-    else
-      BRANDS.push(brand)
-    BRANDS.push(brand)
-    BRANDS.each do |item|
-    #   if item == brand
-    #     puts "exists!"
-    #   else
-    #     BRANDS.push(brand)
-    #   end
-    # end
+    if !BRANDS.include?(brand)
+      BRANDS << brand
+    end
   end
-end
 
   def cobble
     self.condition = "new"
@@ -27,25 +16,6 @@ end
   end
 
 end 
-  def brand=(brand)
-    @brand = brand
-    # BRANDS.push(brand)
-    # BRANDS.push(brand)
-    if BRANDS.include?(brand)
-      puts "exists!"
-    else
-      BRANDS.push(brand)
-    # BRANDS.each do |item|
-    #   if item != brand
-    #     BRANDS.push(brand)
-    #   else
-    #     puts "exists!"
-    #   end
-    # end
-  end
-end
-
-
 
 
 end
